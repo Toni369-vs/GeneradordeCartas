6b9c829cd217615913ac1df1ef27aab5f0fc97b4
Antonio Centenera Mayoral <125910370+Toni369-vs@users.noreply.github.com>
2023-06-07 11:18:23 +0000

    Primeros cambios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function() {};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"];
+const symbols = ["♦", "♥", "♠", "♣"];
+
+// funciones para generar numeros y simbolos aleatorios
+
+function generateRandomValues() {
+  let randomValues = Math.floor(Math.random() * 12);
+  return values[randomValues];
+}
+
+function generateRandomSymbols() {
+  let randomSymbols = Math.floor(Math.random() * 4);
+  if (symbols[randomSymbols] === "♥" || symbols[randomSymbols] === "♦") {
+    symbols[
+      randomSymbols
+    ] = `<span style="color: red;">${symbols[randomSymbols]}</span>`;
+  }
+  return symbols[randomSymbols];
+}
+
+// FIN de funciones para generar numeros y simbolos aleatorios
+
+// Se insertan en HTML los valores generados
+
+let value = generateRandomValues();
+document.getElementById("center").innerHTML = value;
+
+let symbol = generateRandomSymbols();
+document.getElementById("top").innerHTML = symbol;
+document.getElementById("botton").innerHTML = symbol;
+
+// FIN de la inserción
+
+// Funcion crear carta con el boton
+
+document.getElementById("btn").addEventListener("click", generateRandomCard);
+function generateRandomCard() {}
